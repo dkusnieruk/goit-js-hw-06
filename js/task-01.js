90ac0c8cc59887d14c13d9a01b8bc3f4190d7919
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-06 20:37:51 +0200

    update task 1

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+const numberOfCategories = document.querySelectorAll(`li.item`);
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+const firstName =
+document.querySelectorAll(`h2`);
+console.log(`Category :` + firstName[0].textContent);
+const numberElements = 
+document.querySelectorAll(`ul`);
+console.log (`Elements :` + numberElements[1].children.length );
+
+
+
+const secondtName =
+console.log(`Category :` + firstName[1].textContent);
+console.log (`Elements :` + numberElements[2].children.length );
+
+const thirdName =
+console.log(`Category :` + firstName[2].textContent);
+console.log (`Elements :` + numberElements[3].children.length );
