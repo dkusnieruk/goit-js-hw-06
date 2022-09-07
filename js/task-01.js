676bf78c84f35bd26cfb84a37fd4ab1da1bf5410
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-07 05:51:33 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,29 @@
-const numberOfCategories = document.querySelectorAll(`li.item`);
-console.log(`Number of categories: ${numberOfCategories.length}`);
+const categories = document.querySelectorAll(`li.item`);
+console.log(`Number of categories: ` + categories.length);
 
-const firstName =
-document.querySelectorAll(`h2`);
-console.log(`Category :` + firstName[0].textContent);
-const numberElements = 
-document.querySelectorAll(`ul`);
-console.log (`Elements :` + numberElements[1].children.length );
+// const firstName =
+// document.querySelectorAll(`h2`);
+// console.log(`Category :` + firstName[0].textContent);
+// const numberElements = 
+// document.querySelectorAll(`ul`);
+// console.log (`Elements :` + numberElements[1].children.length );
 
 
 
-const secondtName =
-console.log(`Category :` + firstName[1].textContent);
-console.log (`Elements :` + numberElements[2].children.length );
+// const secondtName =
+// console.log(`Category :` + firstName[1].textContent);
+// console.log (`Elements :` + numberElements[2].children.length );
 
-const thirdName =
-console.log(`Category :` + firstName[2].textContent);
-console.log (`Elements :` + numberElements[3].children.length );
+// const thirdName =
+// console.log(`Category :` + firstName[2].textContent);
+// console.log (`Elements :` + numberElements[3].children.length );
+
+const solution = 
+document.querySelectorAll(`li.item`);
+
+solution.forEach(solution => {
+    console.log(`Category : ` + solution.firstElementChild.textContent);
+    console.log(`Elements : ` + solution.getElementsByTagName(`li`).length );
+   
+    
+});
