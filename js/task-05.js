e5c13f2cd4a4e643faa189bdb7dfead3c474dd37
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-12 04:58:29 +0200

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,13 @@
 const textInput = document.querySelector("#name-input");
 
 const textOutput = document.querySelector("#name-output");
-
+const anonymous = "Anonymous";
 
 textInput.addEventListener("input", (event) => {
+    if (event.target.value === "" ) {
+        textOutput.textContent = anonymous;
+    } else 
     textOutput.textContent = event.currentTarget.value;
   });
 
-  
+ 
