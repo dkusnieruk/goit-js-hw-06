07794f7cf066e077624b710ca33c6568b518a2ff
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-13 15:59:00 +0200

    update hw

--- js/task-05.js
+++ js/task-05.js
@@ -7,7 +7,7 @@ textInput.addEventListener("input", (event) => {
     if (event.target.value === "" ) {
         textOutput.textContent = anonymous;
     } else 
-    textOutput.textContent = event.currentTarget.value;
+    {textOutput.textContent = event.currentTarget.value;}
   });
 
  
---
+++ js/task-06.js
@@ -0,0 +1,30 @@
+const solutionInput = document.querySelector(`input`);
+
+const desiredLength = solutionInput.attributes[2].value;
+ 
+
+
+solutionInput.addEventListener( `blur`, (event) =>{
+if (event.currentTarget.value.length === 6)
+{
+    event.currentTarget.setAttribute( `class`, `valid`);
+   // event.target.classList.add(`validation-input.valid`);
+
+}
+else 
+{
+    event.currentTarget.setAttribute( `class`, `invalid`);
+   // event.target.classList.add(`validation-input.invalid`);
+}
+});
+
+
+
+
+
+console.log(solutionInput.attributes[1].value);
+
+console.log(solutionInput.attributes[2].value);
+
+console.log(desiredLength);
+
