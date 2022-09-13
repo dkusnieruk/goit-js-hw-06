82fba53922b8a495f89eff0a221a42b0aaadb684
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-13 19:20:28 +0200

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const getButton = document.querySelector(`button`);
+const getText = document.querySelector(`span`);
+
+
+getButton.addEventListener(`click`, (element) => {
+document.body.style.backgroundColor = getRandomHexColor();
+getText.innerHTML = getRandomHexColor();
+});
+
+console.log(getRandomHexColor());
