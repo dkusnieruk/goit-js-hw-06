91bc3e4207ae63e51aeeb5a7d0ded3020337dcd8
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-13 17:48:30 +0200

    update hw 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const solutionForm = document.querySelector(`form`);
 
+
+solutionForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Uzupełnij wszystkie pola , kolego !");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+// console.log(solutionForm);
