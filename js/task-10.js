7a9db0bf20750b0c0e48dcc7e470a06e6de4e207
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-14 21:33:40 +0200

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,12 +2,32 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const getButton = document.querySelectorAll(`button`)
-const createBox = document.querySelector(`#boxes`);
+const getButton = document.querySelectorAll(`button`);
 
-createBox.addEventListener(`click`, (event)=>{
-  createBox.createElement(`div`);
-})
+const getInput = document.querySelector(`input`);
+
+const getBoxes = document.querySelector(`#boxes`);
 
 
-console.log(createBox);
+
+function createDiv() {
+  for (let i=0; i < getInput.value; i++){
+const solutionDiv= new Array(); 
+solutionDiv[i]=document.createElement(`div`);
+solutionDiv[i].id= `block`;
+solutionDiv[i].style.width = (30 +i*10) + `px`;
+solutionDiv[i].style.height= (30+ i*10) + `px`;
+solutionDiv[i].style.border= `1px solid black`;
+solutionDiv[i].style.backgroundColor = getRandomHexColor();
+getBoxes.appendChild(solutionDiv[i]);
+
+}};
+
+getButton[0].addEventListener(`click`, createDiv);
+
+function destroyDiv(){
+  const solutionBlock =document.getElementById(`#block`);
+  getBoxes.remove(solutionBlock);
+};
+
+getButton[1].addEventListener(`click`, destroyDiv);
